4444e217009caf14b6afcbd5024ed2d8faf10d34
Fabrizio <whoet.wo@gmail.com>
2020-12-29 12:26:18 +0100

    done

---
+++ snack-3/index.php
@@ -0,0 +1,61 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Fabrizio Nicolosi',
+            'text' => 'e4'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Garry Kasparov',
+            'text' => 'e5'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Fabrizio Nicolosi',
+            'text' => 'Cf3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Garry Kasparov',
+            'text' => 'Cc6'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Fabrizio Nicolosi',
+            'text' => 'Ab4'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Garry Kasparov',
+            'text' => ' 1 - 0'
+        ]
+    ],
+];
+?>
+
+<ul>
+      <?php
+      foreach ($posts as $k => $post) {
+         foreach ($post as $details){
+            echo
+            "<li>" .
+             $k .
+             " - " .
+             $details['title'] .
+             " : ".
+             $details['author'] .
+             " - ".
+             $details['text'] .
+             "</li>";
+         }
+      }
+      ?>
+</ul>
